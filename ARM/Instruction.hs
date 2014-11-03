e6e129dbc3ddb6f4d554036979d3575b99cffae0
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2014-11-03 17:20:36 -0500

    Use an Instruction typeclass to consolidate some ARM/Thumb stuff.
    Obviously not done yet but this is what I have to show for today.  I
    don't entirely know what I'm doing and I should probably ask for help
    tomorrow???

---
+++ ARM/Instruction.hs
@@ -0,0 +1,24 @@
+module ARM.Instruction (Instruction(..), getSection) where
+
+import Control.Applicative ((<$>))
+import Data.Binary (Get)
+
+import ARM.Common (Address)
+
+
+class Instruction a where
+    stop :: a -> Bool
+    branchAddress :: a -> Maybe Address
+    printInstruction :: a -> String
+
+getInstruction :: Instruction a => Address -> Get a
+getInstruction = undefined
+
+getSection :: Instruction a => Address -> Get [a]
+getSection sectionStart = do
+    instruction <- getInstruction sectionStart
+
+    if stop instruction then
+        return [instruction]
+    else
+        (instruction :) <$> getSection sectionStart
